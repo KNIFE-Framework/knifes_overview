79877ecb13a6b7c8db9beb094901587b3d1c33a3
roman kazicka <roman.kazicka@systemthinking.xyz>
2025-11-12 13:22:48 +0100

    Deploy 2025-11-12 12:22:48 UTC

---
+++ docs/assets/js/62c88957.60060de4.js
@@ -0,0 +1,11 @@
+"use strict";
+(globalThis["webpackChunkknife_preview"] = globalThis["webpackChunkknife_preview"] || []).push([[7591],{
+
+/***/ 3643:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"categoryGeneratedIndex":{"title":"🌱 7Ds","slug":"/sk/7Ds/overview","permalink":"/knifes_overview/sk/7Ds/overview","sidebar":"docs","navigation":{"previous":{"title":"Krátka história Git – Linus Torvalds","permalink":"/knifes_overview/sk/knifes/K000091-Zrodenie-git-sveta/"},"next":{"title":"00.Navigations","permalink":"/knifes_overview/sk/7Ds/Navigations/"}}}}');
+
+/***/ })
+
+}]);
---
+++ docs/en/assets/js/3ab5f4b9.44cd1844.js
@@ -0,0 +1,11 @@
+"use strict";
+(globalThis["webpackChunkknife_preview"] = globalThis["webpackChunkknife_preview"] || []).push([[6133],{
+
+/***/ 6764:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"categoryGeneratedIndex":{"title":"🌱 7Ds","slug":"/sk/7Ds/overview","permalink":"/knifes_overview/en/sk/7Ds/overview","sidebar":"docs","navigation":{"previous":{"title":"Krátka história Git – Linus Torvalds","permalink":"/knifes_overview/en/sk/knifes/K000091-Zrodenie-git-sveta/"},"next":{"title":"00.Navigations","permalink":"/knifes_overview/en/sk/7Ds/Navigations/"}}}}');
+
+/***/ })
+
+}]);
